ad1d0b45b4df834bab884403bd8fb53a25deee05
elmetal <nm7.pupu@gmail.com>
2026-06-08 16:31:22 +0900

    header

--- LaVoixDuCode/Sources/Layouts/MainLayout.swift
+++ LaVoixDuCode/Sources/Layouts/MainLayout.swift
@@ -4,8 +4,26 @@ import Ignite
 struct MainLayout: Layout {
     var body: some Document {
         Body {
+            SiteNavigation()
             content
             IgniteFooter()
         }
+    }
+}
+
+private struct SiteNavigation: HTML {
+    @Environment(\.site) private var site
+
+    var body: some HTML {
+        NavigationBar(logo: Link(site.name, target: site.sitePath(""))) {
+            Link("Home", target: site.sitePath(""))
+            Link("All posts", target: site.sitePath("tags"))
+            Dropdown("Tags") {
+                Link("Swift", target: site.sitePath("tags/swift"))
+                Link("ぽこあポケモン", target: site.sitePath("tags/pokopia"))
+            }
+        }
+        .navigationItemAlignment(.trailing)
+        .padding(.bottom, .large)
     }
 }
